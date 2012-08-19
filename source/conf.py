4cc72f8f4c12b9139c86d0174faed22a8422b4a3
Tewfik Sadaoui <tsadaoui@work4labs.com>
2012-08-19 16:38:36 +0200

    Configuration générale

--- source/conf.py
+++ source/conf.py
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'Rapportdestage.tex', u'Rapport de stage Documentation',
+  ('index', 'Rapportdestage.tex', u'Rapport de stage',
    u'Tewfik SADAOUI', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'rapportdestage', u'Rapport de stage Documentation',
+    ('index', 'rapportdestage', u'Rapport de stage',
      [u'Tewfik SADAOUI'], 1)
 ]
 
@@ -227,7 +227,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'Rapportdestage', u'Rapport de stage Documentation',
+  ('index', 'Rapportdestage', u'Rapport de stage',
    u'Tewfik SADAOUI', 'Rapportdestage', 'One line description of project.',
    'Miscellaneous'),
 ]
--- source/index.rst
+++ source/index.rst
@@ -3,10 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to Rapport de stage's documentation!
-============================================
-
-Contents:
+Rapport de stage
+================
 
 .. toctree::
    :maxdepth: 2
@@ -18,13 +16,3 @@ Contents:
    stage
    imports
    plate_forme
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
